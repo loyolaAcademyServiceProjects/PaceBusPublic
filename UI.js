e3ebffc62b6f4b2a66aebf5786a49c696ff529c3
Seaver-OlsonLoyola <117297543+Seaver-OlsonLoyola@users.noreply.github.com>
2022-11-02 14:15:32 -0500

    Create UI.js

---
+++ UI.js
@@ -0,0 +1,170 @@
+<style>
+* {
+  box-sizing: border-box;
+}
+body {
+  margin: 0;
+}
+.container-width{
+  width:90%;
+  max-width:1150px;
+  margin:0 auto;
+}
+.blk-sect{
+  padding-top:100px;
+  padding-bottom:100px;
+  background-color:#222222;
+  font-family:Helvetica, serif;
+}
+.blk-title{
+  color:#fff;
+  font-size:25px;
+  text-align:center;
+  margin-bottom:15px;
+}
+.price-cards{
+  margin-top:70px;
+  display:flex;
+  flex-wrap:wrap;
+  align-items:center;
+  justify-content:space-around;
+}
+.price-card-cont{
+  width:300px;
+  padding:7px;
+  float:left;
+}
+.price-card{
+  margin:0 auto;
+  min-height:350px;
+  background-color:#d983a6;
+  border-radius:5px;
+  font-weight:100;
+  color:#fff;
+  width:90%;
+}
+.pc-title{
+  font-weight:100;
+  letter-spacing:3px;
+  text-align:center;
+  font-size:25px;
+  background-color:rgba(0, 0, 0, 0.1);
+  padding:20px;
+}
+.pc-feature{
+  color:rgba(255,255,255,0.5);
+  background-color:rgba(0, 0, 0, 0.1);
+  letter-spacing:2px;
+  font-size:15px;
+  padding:10px 20px;
+}
+.pc-feature:nth-of-type(2n){
+  background-color:transparent;
+}
+.pc-regular{
+  background-color:#da78a0;
+}
+.pc-enterprise{
+  background-color:#d66a96;
+}
+.gjs-row{
+  display:flex;
+  justify-content:flex-start;
+  align-items:stretch;
+  flex-wrap:nowrap;
+  padding:10px;
+}
+.gjs-cell{
+  min-height:75px;
+  flex-grow:1;
+  flex-basis:100%;
+}
+#i1oi4{
+  padding:10px;
+}
+#i3np2{
+  padding:10px;
+}
+#ixpxs{
+  padding:10px;
+}
+#i8jt6{
+  padding:10px;
+}
+#iajax{
+  padding:10px;
+}
+#i7jfh{
+  padding:10px;
+}
+@media (max-width: 768px){
+  .gjs-row{
+    flex-wrap:wrap;
+  }
+}
+
+</style>
+<body id="inil">
+  <section class="blk-sect" id="i2vxi">
+    <div class="container-width">
+      <div class="blk-title" id="i2p6n">Schedule
+      </div>
+      <div class="price-cards" id="ipdzfu">
+        <div class="price-card-cont" id="iqohc7">
+          <div class="price-card" id="iju6kk">
+            <div class="pc-title" id="izt2pl">421
+            </div>
+            <div class="gjs-row" id="icdnr">
+              <div class="gjs-cell" id="ikauk">
+                <div id="i1oi4">West:$Time
+                </div>
+              </div>
+              <div class="gjs-cell" id="izing">
+                <div id="i3np2">East:$Time
+                </div>
+              </div>
+            </div>
+            <div class="pc-feature" id="ijyvo4">- On Time
+            </div>
+          </div>
+        </div>
+        <div class="price-card-cont">
+          <div class="price-card pc-regular">
+            <div class="pc-title" id="iqujdn">422
+            </div>
+            <div class="gjs-row" id="ikxm7">
+              <div class="gjs-cell" id="is7yk">
+                <div id="ixpxs">West:$Time
+                </div>
+              </div>
+              <div class="gjs-cell" id="i4epu">
+                <div id="i8jt6">East:$Time
+                </div>
+              </div>
+            </div>
+            <div class="pc-feature" id="ieg9st">- On Time
+            </div>
+          </div>
+        </div>
+        <div class="price-card-cont">
+          <div class="price-card pc-enterprise" id="i3vryv">
+            <div class="pc-title" id="i971qu">423
+            </div>
+            <div class="gjs-row" id="icc2m">
+              <div class="gjs-cell" id="ib7oa">
+                <div id="iajax">West:$Time
+                </div>
+              </div>
+              <div class="gjs-cell" id="iwuti">
+                <div id="i7jfh">East:$Time
+                </div>
+              </div>
+            </div>
+            <div class="pc-feature" id="i40s2q">- On Time
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</body>
